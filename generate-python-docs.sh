b0246adfa9fefa5913cac4210550dca5159a762b
Greg Schoeninger <greg.schoeninger@gmail.com>
2024-02-29 19:07:30 -0800

    added py doc strings for python diff

--- generate-python-docs.sh
+++ generate-python-docs.sh
@@ -5,18 +5,29 @@ DOCS_DIR=$1
 
 # Define your list
 list=(
-    "clone"
-    "init"
     "auth"
-    "user"
-    "remote_repo"
+    "clone"
+    "config"
+    "df"
+    "diff/diff"
+    "diff/tabular_diff"
+    "diff/line_diff"
+    "diff/text_diff"
+    "init"
     "local_repo"
+    "remote_repo"
+    "streaming_dataset"
+    "user"
 )
 
 # Loop over the list
 for i in "${list[@]}"; do
     echo "Generating docs for $i in python-api/$i.mdx"
+    # make parent dir if not exists $DOCS_DIR/python-api/$i.mdx
+    mkdir -p $DOCS_DIR/python-api/$(dirname $i)
+
     pydoc-markdown -I python -m oxen.$i --no-render-toc > $DOCS_DIR/python-api/$i.mdx
+
     # for our docs we want the class methods to be top level in the nav
     gsed -i "s:#### :## :g" $DOCS_DIR/python-api/$i.mdx
 done
